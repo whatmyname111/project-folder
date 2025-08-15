89616248259aeb10c8e3c9dd8ddaa289d99aa9f8
whatmyname111 <m00263277@gmail.com>
2025-08-15 16:10:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ ERR_SAVE_KEY = 'Failed to save key'
 # Flask app
 app = Flask(__name__)
 app.secret_key = os.getenv("ADMIN_SESSION_KEY")  # секрет для сессий
-app.config['SESSION_COOKIE_NAME'] = os.dotenv("sskk")
+app.config['SESSION_COOKIE_NAME'] = os.getenv("sskk")
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)
 CORS(app, resources={r"/api/*": {"origins": ["https://www.roblox.com", "https://*.robloxlabs.com"]}})
 limiter = Limiter(get_remote_address, app=app, default_limits=['20 per minute'])
